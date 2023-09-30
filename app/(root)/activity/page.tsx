f900d2756183b1d22a25a162903c9694c862c0e8
DeepDarkFantasy <will091287@gmail.com>
2023-09-30 16:46:22 +0800

    Search Page

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,11 @@
+
+
+const page = () => {
+  return (
+    <section>
+      <h1 className="head-text">Activity</h1>
+    </section>
+  )
+}
+
+export default page
---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,51 @@
+import ThreadCard from "@/components/cards/ThreadCard";
+import UserCard from "@/components/cards/UserCard";
+import Comment from "@/components/forms/Comment";
+import { fetchThreadById } from "@/lib/actions/thread.actions";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const page = async () => {
+  const user = await currentUser();
+  if(!user) return null;
+  
+  const userInfo = await fetchUser(user.id);
+  if(!userInfo?.onboarded) redirect('/onboarding')
+
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: '',
+    pageNumber: 1,
+    pageSize: 25,
+  });
+
+  return (
+    <section>
+      <h1 className="head-text">Search</h1>
+
+      {/* Search Bar */}
+
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result">No users</p>
+        ) : (
+          <>
+            {result.users.map((person: any) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
+    </section>
+  )
+}
+
+export default page
