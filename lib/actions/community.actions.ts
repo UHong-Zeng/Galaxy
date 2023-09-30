5c2bc8d557c530b04fee05b93db9e99f5896decb
DeepDarkFantasy <will091287@gmail.com>
2023-09-30 23:44:11 +0800

    Edit Community Action

--- lib/actions/community.actions.ts
+++ lib/actions/community.actions.ts
@@ -289,7 +289,7 @@ export async function deleteCommunity(communityId: string) {
     const communityUsers = await User.find({ communities: communityId });
 
     // Remove the community from the 'communities' array for each user
-    const updateUserPromises = communityUsers.map((user: any) => {
+    const updateUserPromises = communityUsers.map((user) => {
       user.communities.pull(communityId);
       return user.save();
     });
